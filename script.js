ba4045a873e14fd16d80debda7d133ebc8044a5a
Damion Shirkey <shirkeydamion@gmail.com>
2026-08-12 23:00:31 -0500

    Break out book card creation logic

--- script.js
+++ script.js
@@ -58,60 +58,63 @@ function updateRead(bookId) {
     }
 }
 
+function buildBookCard(book) {
+    const bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "book card");
+    bookCard.setAttribute("data-id", book.id);
+
+    const bookRemoveButton = document.createElement("button");
+    bookRemoveButton.setAttribute("class", "button book__remove");
+    bookRemoveButton.textContent = "X";
+    bookCard.appendChild(bookRemoveButton);
+
+    const bookCardTitle = document.createElement("h2");
+    bookCardTitle.setAttribute("class", "book__title")
+    bookCardTitle.textContent = book.title;
+    bookCard.appendChild(bookCardTitle);
+
+    const bookCardAuthor = document.createElement("p");
+    bookCardAuthor.setAttribute("class", "book__author")
+    bookCardAuthor.textContent = `Author: ${book.author}`;
+    bookCard.appendChild(bookCardAuthor);
+
+    const bookCardPublishYear = document.createElement("p");
+    bookCardPublishYear.setAttribute("class", "book__publish-year")
+    bookCardPublishYear.textContent = `Published: ${book.publishYear}`;
+    bookCard.appendChild(bookCardPublishYear);
+
+    const bookCardPages = document.createElement("p");
+    bookCardPages.setAttribute("class", "book__pages");
+    bookCardPages.textContent = `Pages: ${book.pages}`;
+    bookCard.appendChild(bookCardPages);
+
+    const bookCardReadContainer = document.createElement("div");
+    const bookCardRead = document.createElement("p");
+    bookCardRead.textContent = "Read:";
+    bookCardRead.setAttribute("class", "book__read");
+    bookCardReadContainer.appendChild(bookCardRead);
+
+    const bookCardReadCheckbox = document.createElement("input");
+    bookCardReadCheckbox.setAttribute("type", "checkbox");
+    bookCardReadCheckbox.setAttribute("class", "book__read-checkbox");
+    bookCardReadCheckbox.setAttribute("disabled", true);
+    bookCardReadCheckbox.checked = book.read;
+    bookCardReadContainer.appendChild(bookCardReadCheckbox);
+
+    bookCard.appendChild(bookCardReadContainer);
+
+    const updateReadButton = document.createElement("button");
+    updateReadButton.setAttribute("class", "button book__update-read");
+    updateReadButton.textContent = "Update Read Status";
+    bookCard.appendChild(updateReadButton);
+
+    libraryGrid.appendChild(bookCard);
+}
+
 function displayLibrary() {
     libraryGrid.textContent = "";
-
     for (const book of library) {
-        const bookCard = document.createElement("div");
-        bookCard.setAttribute("class", "book card");
-        bookCard.setAttribute("data-id", book.id);
-
-        const bookRemoveButton = document.createElement("button");
-        bookRemoveButton.setAttribute("class", "button book__remove");
-        bookRemoveButton.textContent = "X";
-        bookCard.appendChild(bookRemoveButton);
-
-        const bookCardTitle = document.createElement("h2");
-        bookCardTitle.setAttribute("class", "book__title")
-        bookCardTitle.textContent = book.title;
-        bookCard.appendChild(bookCardTitle);
-
-        const bookCardAuthor = document.createElement("p");
-        bookCardAuthor.setAttribute("class", "book__author")
-        bookCardAuthor.textContent = `Author: ${book.author}`;
-        bookCard.appendChild(bookCardAuthor);
-
-        const bookCardPublishYear = document.createElement("p");
-        bookCardPublishYear.setAttribute("class", "book__publish-year")
-        bookCardPublishYear.textContent = `Published: ${book.publishYear}`;
-        bookCard.appendChild(bookCardPublishYear);
-
-        const bookCardPages = document.createElement("p");
-        bookCardPages.setAttribute("class", "book__pages");
-        bookCardPages.textContent = `Pages: ${book.pages}`;
-        bookCard.appendChild(bookCardPages);
-
-        const bookCardReadContainer = document.createElement("div");
-        const bookCardRead = document.createElement("p");
-        bookCardRead.textContent = "Read:";
-        bookCardRead.setAttribute("class", "book__read");
-        bookCardReadContainer.appendChild(bookCardRead);
-
-        const bookCardReadCheckbox = document.createElement("input");
-        bookCardReadCheckbox.setAttribute("type", "checkbox");
-        bookCardReadCheckbox.setAttribute("class", "book__read-checkbox");
-        bookCardReadCheckbox.setAttribute("disabled", true);
-        bookCardReadCheckbox.checked = book.read;
-        bookCardReadContainer.appendChild(bookCardReadCheckbox);
-
-        bookCard.appendChild(bookCardReadContainer);
-
-        const updateReadButton = document.createElement("button");
-        updateReadButton.setAttribute("class", "button book__update-read");
-        updateReadButton.textContent = "Update Read Status";
-        bookCard.appendChild(updateReadButton);
-
-        libraryGrid.appendChild(bookCard);
+        buildBookCard(book);
     }
 }
 
